f2c9cc1da14b77f669d0b110443698c78f6f8c55
Joshua Goldberg <jsgoldberg90@gmail.com>
2015-08-03 11:34:40 -0400

    initial commit

---
+++ r-bind.js
@@ -0,0 +1,9 @@
+
+var rBind = function(func, context, args){
+	return function(){
+		var origArgs = Array.prototype.slice.call(arguments);
+		return func.apply(context, origArgs.concat(args));
+	};
+};
+
+module.exports = {rBind: rBind};
